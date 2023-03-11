fc33734f4f36ab623925512e3cd1b81019b027c2
Daniel Sacro <dbsacro@gmail.com>
2023-03-11 15:01:01 -0700

    Database now remembers which users a code has been scanned by

--- android_files/app/src/main/java/com/example/qradventure/ScanFragment.java
+++ android_files/app/src/main/java/com/example/qradventure/ScanFragment.java
@@ -50,6 +50,8 @@ public class ScanFragment extends Fragment implements DisplayCodePromptPictureFr
 
     CollectionReference userCodes;
 
+    CollectionReference dbCodes;
+
     // So fragment appears in order
     @Override
     public void openCameraInScanFrag(Boolean state) {
@@ -103,7 +105,7 @@ public class ScanFragment extends Fragment implements DisplayCodePromptPictureFr
                     code = new QRCode(result.getContents());
 
                     // Save code if user doesn't have, otherwise don't save
-                    handleCodeSaving(); // updates userHasCode
+                    trySaveCodeToUser();
                 }
             });
 
@@ -155,9 +157,12 @@ public class ScanFragment extends Fragment implements DisplayCodePromptPictureFr
                              Bundle savedInstanceState) {
         // Instantiate user data
         user = user.getInstance();
-        userCodes = db
-                .collection("Users").document(user.getUserPhoneID())
+
+        // Instantiate data from database
+        userCodes = db.collection("Users").document(user.getUserPhoneID())
                 .collection("Codes");
+
+        dbCodes = db.collection("QRCodes");
 
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_scan, container, false);
@@ -179,20 +184,23 @@ public class ScanFragment extends Fragment implements DisplayCodePromptPictureFr
 
     }
 
-    private void handleCodeSaving() {
+    private void trySaveCodeToUser() {
         // https://firebase.google.com/docs/firestore/query-data/get-data#java_2
         DocumentReference docRef = userCodes.document(code.getHashValue());
+
+        // Check if user has code already
         docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
             public void onComplete(@NonNull Task<DocumentSnapshot> task) {
                 if (task.isSuccessful()) {
                     DocumentSnapshot document = task.getResult();
                     if (document.exists()) {
-                        // If user has it, don't save and update flag
+                        // If user has it, don't save it and show code anyways
                         displayCodeAndPromptPicture(true);
+
                         Log.d(TAG, "DocumentSnapshot data: " + document.getData());
                     } else {
-                        // If user doesn't have it, save and update flag
+                        // If user doesn't have it, save it (associate code with user)
                         Map<String, Object> newCode = new HashMap<>();
                         newCode.put(code.getHashValue(), 1); // Object value doesn't matter
                         docRef.set(newCode)
@@ -208,7 +216,13 @@ public class ScanFragment extends Fragment implements DisplayCodePromptPictureFr
                                         Log.w(TAG, "Error writing document", e);
                                     }
                                 });
+
+                        // Save it again (associate user with code)
+                        saveCodeToDb();
+
+                        // Show code
                         displayCodeAndPromptPicture(false);
+
                         Log.d(TAG, "No such document");
                     }
                 } else {
@@ -216,6 +230,76 @@ public class ScanFragment extends Fragment implements DisplayCodePromptPictureFr
                 }
             }
         });
+    }
+
+    private void saveCodeToDb() {
+        // Almost same code as saveCodeToUser()
+        DocumentReference docRef = dbCodes.document(code.getHashValue());
+
+        // Check if code exists in db
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        // If code exists, save user to code
+                        saveUserToCode();
+                        Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+                    } else {
+                        // If code doesn't exist, save it to db
+                        Map<String, Object> newCode = new HashMap<>();
+                        newCode.put(code.getHashValue(), 1); // Object value doesn't matter
+                        docRef.set(newCode)
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        Log.d(TAG, "DocumentSnapshot successfully written!");
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Log.w(TAG, "Error writing document", e);
+                                    }
+                                });
+
+                        // Code exists in db now, so save user to code
+                        saveUserToCode();
+
+                        Log.d(TAG, "No such document");
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
+    }
+
+    private void saveUserToCode() {
+        // This function is only accessed if user doesn't have code in trySaveCodeToUser()
+        // Don't need to check if code has user again
+
+        DocumentReference docRef = dbCodes.document(code.getHashValue())
+                .collection("Users").document(user.getUserPhoneID());
+
+        // Associate user to code
+        Map<String, Object> newUser = new HashMap<>();
+        newUser.put(user.getUserPhoneID(), 1); // Object value doesn't matter
+        docRef.set(newUser)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "DocumentSnapshot successfully written!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error writing document", e);
+                    }
+                });
+
     }
 
     // Sets up and opens scanner
@@ -226,6 +310,11 @@ public class ScanFragment extends Fragment implements DisplayCodePromptPictureFr
         options.setPrompt("Scan a QR Code!");
         options.setDesiredBarcodeFormats(ScanOptions.QR_CODE);
         fragmentLauncher.launch(options);
+    }
+
+    public void openCamera() {
+        Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        launcher.launch(camera);
     }
 
     private void displayCodeAndPromptPicture(boolean isSeen) {
@@ -244,10 +333,5 @@ public class ScanFragment extends Fragment implements DisplayCodePromptPictureFr
     private void promptGeolocation() {
         PromptGeolocationFragment frag = new PromptGeolocationFragment(code);
         frag.show(getActivity().getSupportFragmentManager(), "Prompt geolocation");
-    }
-
-    public void openCamera() {
-        Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        launcher.launch(camera);
     }
 }
