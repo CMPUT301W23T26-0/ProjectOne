5afd930950b90a6d2803ec5081ec092aad4f79a5
Daniel Sacro <dbsacro@gmail.com>
2023-03-11 22:13:54 -0700

    Geolocation now gets saved to database from scanner

--- android_files/app/src/main/java/com/example/qradventure/PromptGeolocationFragment.java
+++ android_files/app/src/main/java/com/example/qradventure/PromptGeolocationFragment.java
@@ -2,11 +2,17 @@ package com.example.qradventure;
 
 import static android.content.ContentValues.TAG;
 
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.Looper;
+import android.provider.Settings;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
@@ -15,8 +21,15 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -33,6 +46,8 @@ public class PromptGeolocationFragment extends DialogFragment {
     CollectionReference dbCodes;
 
     private QRCode code;
+
+    FusedLocationProviderClient client;
 
     public PromptGeolocationFragment(QRCode code) {
         this.code = code;
@@ -54,9 +69,11 @@ public class PromptGeolocationFragment extends DialogFragment {
                             @Override
                             public void onClick(DialogInterface dialogInterface, int i) {
                                 // Record geolocation to QRCode
-                                Location geolocation = user.getCurrentLocation();
-                                geolocation = null;
-                                saveGeolocation(geolocation);
+                                client = LocationServices
+                                        .getFusedLocationProviderClient(
+                                                getActivity());
+
+                                getCurrentLocation();
                                 toastMessage = "Saving geolocation...";
                             }
                         })
@@ -65,11 +82,96 @@ public class PromptGeolocationFragment extends DialogFragment {
                             @Override
                             public void onClick(DialogInterface dialogInterface, int i) {
                                 // Don't record geolocation
-//                                saveGeolocation(null);
                             }
                         });
 
         return builder.create();
+    }
+
+    // https://www.geeksforgeeks.org/how-to-get-current-location-inside-android-fragment/
+    @SuppressLint("MissingPermission")
+    private void getCurrentLocation()
+    {
+        // Initialize Location manager
+        LocationManager locationManager
+                = (LocationManager)getActivity()
+                .getSystemService(
+                        Context.LOCATION_SERVICE);
+        // Check condition
+        if (locationManager.isProviderEnabled(
+                LocationManager.GPS_PROVIDER)
+                || locationManager.isProviderEnabled(
+                LocationManager.NETWORK_PROVIDER)) {
+            // When location service is enabled
+            // Get last location
+            client.getLastLocation().addOnCompleteListener(
+                    new OnCompleteListener<Location>() {
+                        @Override
+                        public void onComplete(
+                                @NonNull Task<Location> task)
+                        {
+
+                            // Initialize location
+                            Location location
+                                    = task.getResult();
+                            // Check condition
+                            if (location != null) {
+                                // When location result is not
+                                // null set latitude
+                                saveGeolocation(location);
+                            }
+                            else {
+                                // When location result is null
+                                // initialize location request
+                                LocationRequest locationRequest
+                                        = new LocationRequest()
+                                        .setPriority(
+                                                LocationRequest
+                                                        .PRIORITY_HIGH_ACCURACY)
+                                        .setInterval(10000)
+                                        .setFastestInterval(
+                                                1000)
+                                        .setNumUpdates(1);
+
+                                // Initialize location call back
+                                LocationCallback
+                                        locationCallback
+                                        = new LocationCallback() {
+                                    @Override
+                                    public void
+                                    onLocationResult(
+                                            LocationResult
+                                                    locationResult)
+                                    {
+                                        // Initialize
+                                        // location
+                                        Location currLocation
+                                                = locationResult
+                                                .getLastLocation();
+
+                                        saveGeolocation(currLocation);
+                                    }
+                                };
+
+                                // Request location updates
+                                client.requestLocationUpdates(
+                                        locationRequest,
+                                        locationCallback,
+                                        Looper.myLooper());
+                            }
+                        }
+                    });
+        }
+        else {
+            // When location service is not enabled
+            // open location setting
+            startActivity(
+                    new Intent(
+                            Settings
+                                    .ACTION_LOCATION_SOURCE_SETTINGS)
+                            .setFlags(
+                                    Intent.FLAG_ACTIVITY_NEW_TASK));
+        }
     }
 
     private void saveGeolocation(Location geolocation) {
