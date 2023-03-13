0551d34fb2938168c80c8152a7fd2310da861989
Abrar <azahin1@ualberta.ca>
2023-03-13 01:19:08 -0600

    Add QR passed

--- android_files/app/src/androidTest/java/com/example/qradventure/MainActivityTest.java
+++ android_files/app/src/androidTest/java/com/example/qradventure/MainActivityTest.java
@@ -1,24 +1,19 @@
 package com.example.qradventure;
 
-import android.app.Activity;
 
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 import androidx.test.rule.ActivityTestRule;
-
-import android.provider.Settings;
 import android.util.Log;
-import android.widget.EditText;
-import android.widget.ListView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QuerySnapshot;
-import com.google.firebase.firestore.core.FirestoreClient;
 import com.robotium.solo.Solo;
 
 import org.junit.After;
@@ -30,6 +25,10 @@ import org.junit.runner.RunWith;
 import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Test class for MainActivity. All the UI tests are written here. Robotium test framework is used
  */
@@ -55,8 +54,9 @@ public class MainActivityTest{
         user = UserDataClass.getInstance();
         TAG = "TEST: ";
     }
+
     /**
-     * Gets the Activity
+     * Tests if login screen appears properly
      * @throws Exception
      */
     @Test
@@ -79,10 +79,24 @@ public class MainActivityTest{
         });
 
         if (loggedIn[0]) {
-            solo.assertCurrentActivity("Wrong Activity" ,MainActivity.class);
+            solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
         } else {
             solo.assertCurrentActivity("Wrong Activity", LoginActivity.class);
         }
+    }
+
+    @Test
+    public void addQR() {
+        String id = user.getUserPhoneID();
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+
+        QRCode code = new QRCode("028cdf2261a35d620d7a4ecd9466cb3ba5d39dee006a86e7483a8dcbdf695695");
+        CollectionReference cRef = db.collection("Users").document(id).collection("Codes");
+        Map<String, Object> newCode = new HashMap<>();
+        newCode.put("name", code.getName());
+        newCode.put("score", code.getScore());
+        newCode.put("hash", code.getHashValue());
+        cRef.add(newCode);
     }
 
     /**
