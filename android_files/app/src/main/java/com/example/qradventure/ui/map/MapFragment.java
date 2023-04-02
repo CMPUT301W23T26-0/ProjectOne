f83641c04e86f2582094e0bc5d79cbee9766fb5d
Carl Rogador <rogador@ualberta.ca>
2023-04-01 20:08:11 -0600

    Change camera behavior on nearby QR code dialog click

--- android_files/app/src/main/java/com/example/qradventure/ui/map/MapFragment.java
+++ android_files/app/src/main/java/com/example/qradventure/ui/map/MapFragment.java
@@ -350,8 +350,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback{
     private void initializeLocation() {
         Location userLocation = user.getCurrentLocation();
         LatLng userLatLng = new LatLng(userLocation.getLatitude(), userLocation.getLongitude());
-        mMap.moveCamera(CameraUpdateFactory.zoomTo(16));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(userLatLng));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLatLng, 16));
     }
 
     /**
@@ -438,7 +437,7 @@ public class MapFragment extends Fragment implements OnMapReadyCallback{
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 LatLng tempLocation = locationDict.get(qrCodeList[which]);
-                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(tempLocation, 16));
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(tempLocation, 16));
             }
         };
 
