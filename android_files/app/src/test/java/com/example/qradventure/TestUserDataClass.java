dd5050fef2a879e0a22ecfc5d76f8133f3d96f0a
Yuri Sanchez <yds123001@gmail.com>
2023-04-02 19:03:40 -0600

    added skeleton code for unit testing

---
+++ android_files/app/src/test/java/com/example/qradventure/TestUserDataClass.java
@@ -0,0 +1,78 @@
+package com.example.qradventure;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import com.example.qradventure.users.UserDataClass;
+import com.google.firebase.firestore.auth.User;
+
+public class TestUserDataClass {
+    UserDataClass user = UserDataClass.getInstance();
+
+    @Test
+    public void testSetData() {
+
+    }
+
+    @Test
+    public void testPhoneInfo() {
+
+    }
+
+    @Test
+    public void testEmailInfo() {
+
+    }
+
+    @Test
+    public void testUsername() {
+
+    }
+    @Test
+    public void testTotalScore() {
+
+    }
+
+    @Test
+    public void testUserRef() {
+
+    }
+
+    @Test
+    public void testUserCodesRef() {
+
+    }
+    @Test
+    public void testHighestQrScore() {
+
+    }@Test
+    public void testHighestQrHash() {
+
+    }
+    @Test
+    public void testHighestQr() {
+
+    }
+
+    @Test
+    public void testAddCode() {
+
+    }
+
+    @Test
+    public void testDeleteCode() {
+
+    }
+
+    @Test
+    public void testUpdateField() {
+
+    }
+
+    @Test
+    public void testRefreshHighestQr() {
+
+    }
+
+
+}
